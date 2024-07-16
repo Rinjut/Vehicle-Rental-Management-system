e4065b061b545ef7a18af4c2cdfad3864c195a88
Rinjut <Rthankammamathe9698@conestogac.on.ca>
2024-07-15 21:40:57 -0400

    Truck and motrocycle files created.Properties added

---
+++ Vehicle Rental Management system/Truck.cs
@@ -0,0 +1,32 @@
+﻿public class Truck : Vehicle
+{
+    private int capacity;
+    private string truckType;
+    private bool fourWheelDrive;
+
+    public int Capacity
+    {
+        get { return capacity; }
+        set { capacity = value; }
+    }
+
+    public string TruckType
+    {
+        get { return truckType; }
+        set { truckType = value; }
+    }
+
+    public bool FourWheelDrive
+    {
+        get { return fourWheelDrive; }
+        set { fourWheelDrive = value; }
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Capacity:"+Capacity);
+        Console.WriteLine("Truck Type:"+TruckType);
+        Console.WriteLine("Four Wheel Drive:"+FourWheelDrive);
+    }
+}
